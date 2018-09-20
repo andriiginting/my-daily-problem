d370006650e08a8b9c19490fd036b3776966b164
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-09-20 17:19:50 +0700

    [andri] update solutions

--- src/main/java/PlusMinus.java
+++ src/main/java/PlusMinus.java
@@ -1,17 +1,17 @@
 public class PlusMinus {
-    static void solution(int[] arr){
+    public void solution(int[] arr){
         int negative = 0;
         int zeros = 0;
         int positive = 0;
         int length = arr.length;
 
-        for(int i = 0 ; i<arr.length; i++){
-            if(arr[i] < 0){
-                negative+=1;
-            }else if(arr[i] == 0){
-                zeros+=1;
-            }else if(arr[i] > 0){
-                positive+=1;
+        for (int anArr : arr) {
+            if (anArr < 0) {
+                negative += 1;
+            } else if (anArr == 0) {
+                zeros += 1;
+            } else {
+                positive += 1;
             }
         }
 
