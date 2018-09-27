1c590ad3d2c0e938c173249a023312651f9234bd
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-09-27 15:19:29 +0700

    [andri] add apple and orange problem solution

---
+++ src/main/java/AppleAndOranger.java
@@ -0,0 +1,20 @@
+public class AppleAndOranger {
+    public void countAppleAndOrange(int s, int t, int a, int b, int[] apples, int[] oranges){
+        int orangeCount = 0;
+        int appleCount = 0;
+        for (int apple : apples) {
+            if (a + apple >= s && a + apple <= t) {
+                appleCount++;
+            }
+        }
+
+        for (int orange : oranges) {
+            if (b + orange >= s && b + orange <= t) {
+                orangeCount++;
+            }
+        }
+
+        System.out.println(appleCount);
+        System.out.println(orangeCount);
+    }
+}
