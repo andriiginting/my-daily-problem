2f3a6a409b71d6934d71cf69f606f87ca50e5d4d
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2018-10-10 19:36:49 +0700

    [Arifin Firdaus] Rename class

--- src/main/java/BreakingTheRecords.java
+++ src/main/java/BreakingTheRecords.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class BreakingTheRecords {
 
   // Count
   // Game   Score  Minimum  Maximum     Min Max
